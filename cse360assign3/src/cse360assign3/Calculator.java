f5c976aeab03f1a9f72eb46e243f22e643ec66ae
bhavana chinta <bchinta@asu.edu>
2016-02-21 15:28:35 -0700

    perfect!

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,8 +1,16 @@
+/** URL: **/
+/** Name: Bhavana Chinta **/
+/** Pin-216**/
+/**Assignment-3**/
+
 package cse360assign3;
 
 public class Calculator {
 private int total;
-	
+private String 	number;
+private String sign; 
+private String concatinate= "0";
+
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
@@ -13,26 +21,43 @@ private int total;
 	
 	public void add (int value) {
 		total+= value;
+		number = Integer.toString (value);
+		sign="+";
+		concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public void subtract (int value) {
 		total -= value;
+		number = Integer.toString (value);
+	    sign="–";
+	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public void multiply (int value) {
 		
 		total *= value ;
+		number = Integer.toString (value);
+	    sign="*";
+	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public void divide (int value) {
 		
-		int div = total/value;
 		if(value==0)
-			total=0;
+		{	total=0; }
+		else
+		{
+			int div = total/value;
+		}
+		number = Integer.toString (value);
+	    sign="/";
+	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public String getHistory () {
-		return "";
+		
+		/**concatinate= concatinate+" "+sign+" "+number;**/
+		return concatinate;
 	}
 
 }
