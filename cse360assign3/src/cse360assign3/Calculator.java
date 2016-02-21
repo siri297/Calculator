84b9f221ac962094bbc8bfec1f6aa7f9eedfb102
bhavana chinta <bchinta@asu.edu>
2016-02-21 15:53:04 -0700

    Perfect with comments and URL

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,4 +1,4 @@
-/** URL: **/
+/** URL: https://github.com/siri297/Calculator/blob/master/cse360assign3/src/cse360assign3/Calculator.java **/
 /** Name: Bhavana Chinta **/
 /** Pin-216**/
 /**Assignment-3**/
@@ -7,7 +7,8 @@ package cse360assign3;
 
 public class Calculator {
 private int total;
-private String 	number;
+/** Two string global variables are declared and these variables used to store the integer value and also assign the symbols "+,-,/,*" **/
+private String 	number; 
 private String sign; 
 private String concatinate= "0";
 
@@ -20,44 +21,43 @@ private String concatinate= "0";
 	}
 	
 	public void add (int value) {
-		total+= value;
-		number = Integer.toString (value);
-		sign="+";
-		concatinate= concatinate+" "+sign+" "+number;
+		total+= value; /** the value plugged in is added to the total and the total holds added value **/
+		number = Integer.toString (value); /** storing the integer**/
+		sign="+";        /** assigned the symbol of addition to sign **/
+		concatinate= concatinate+" "+sign+" "+number; 
 	}
 	
 	public void subtract (int value) {
-		total -= value;
-		number = Integer.toString (value);
-	    sign="–";
+		total -= value;         /** the value plugged in is subtracted to the total and the total holds subtracted value **/
+		number = Integer.toString (value);  /** storing the integer**/
+	    sign="–";               /** assigned the symbol of subtraction to sign **/
 	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public void multiply (int value) {
 		
-		total *= value ;
-		number = Integer.toString (value);
-	    sign="*";
+		total *= value ; /** the value plugged in is multiplied to the total and the total holds multiplied value **/
+		number = Integer.toString (value);     /** storing the integer**/
+	    sign="*";                /** assigned the symbol of multiplication to sign **/
 	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public void divide (int value) {
-		
+		/** if the value inserted is 0 then it goes in to the if condition and sets the total to 0 or it goes to else**/
 		if(value==0)
-		{	total=0; }
+		{	total=0; } /** this is what it happens in the test case called divide_by_zero**/ 
 		else
 		{
-			int div = total/value;
+			total = total/value;     /** now the value is divided and stored it in total**/
 		}
-		number = Integer.toString (value);
-	    sign="/";
+		number = Integer.toString (value);      /** storing the integer**/
+	    sign="/";           /** assigned the symbol of division to sign **/
 	    concatinate= concatinate+" "+sign+" "+number;
 	}
 	
 	public String getHistory () {
 		
-		/**concatinate= concatinate+" "+sign+" "+number;**/
-		return concatinate;
+		return concatinate; /** the concatinate contains the concatinate value plus the sign plus the number so, thats what the getHistory() test case is expected **/
 	}
 
 }
